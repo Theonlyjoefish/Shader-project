e3e447f10aed6758ced07885882dcd23c243e0b3
Joseph Wiltshire <wiltshirejoseph@hotmail.co.uk>
2018-10-26 15:09:57 +0100

    Added the code for draw, update and init.

--- Shaders/Shaders/Main.cpp
+++ Shaders/Shaders/Main.cpp
@@ -57,6 +57,10 @@ float attConstant = 1.0f;
 float attLinear = 0.0f;
 float attQuadratic = 0.0f;
 
+GLuint gouraudShader;
+GLuint texture;
+GLuint skyboxShader;
+
 SDL_Window * setupRC(SDL_GLContext &context)
 {
 	SDL_Window * window;
@@ -82,19 +86,177 @@ SDL_Window * setupRC(SDL_GLContext &context)
 	return window;
 }
 
+GLuint loadCubeMap(const char *fname[6], GLuint *texID)
+{
+	glGenTextures(1, texID);
+	GLenum sides[6] = {
+		GL_TEXTURE_CUBE_MAP_POSITIVE_Z,
+		GL_TEXTURE_CUBE_MAP_NEGATIVE_Z,
+		GL_TEXTURE_CUBE_MAP_POSITIVE_X,
+		GL_TEXTURE_CUBE_MAP_NEGATIVE_X,
+		GL_TEXTURE_CUBE_MAP_POSITIVE_Y,
+		GL_TEXTURE_CUBE_MAP_NEGATIVE_Y
+	};
+	SDL_Surface *tmpSurface;
+
+	glBindTexture(GL_TEXTURE_CUBE_MAP, *texID);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+	GLuint externalFormat;
+	for (int i = 0; i < 6; i++)
+	{
+		
+		tmpSurface = SDL_LoadBMP(fname[i]);
+		if (!tmpSurface)
+		{
+			std::cout << "Error loading bitmap" << std::endl;
+			return *texID;
+		}
+
+		
+		SDL_PixelFormat *format = tmpSurface->format;
+		externalFormat = (format->Rmask < format->Bmask) ? GL_RGB : GL_BGR;
+
+		glTexImage2D(sides[i], 0, GL_RGB, tmpSurface->w, tmpSurface->h, 0,
+			externalFormat, GL_UNSIGNED_BYTE, tmpSurface->pixels);
+		
+		SDL_FreeSurface(tmpSurface);
+	}
+	return *texID;
+}
+
+GLuint loadBitmap(const char *fname) {
+	GLuint texID;
+	glGenTextures(1, &texID); 
+
+							  
+	SDL_Surface *tmpSurface;
+	tmpSurface = SDL_LoadBMP(fname);
+	if (!tmpSurface) {
+		std::cout << "Error loading bitmap" << std::endl;
+	}
+
+	
+	glBindTexture(GL_TEXTURE_2D, texID);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+	SDL_PixelFormat *format = tmpSurface->format;
+
+	GLuint externalFormat, internalFormat;
+	if (format->Amask) {
+		internalFormat = GL_RGBA;
+		externalFormat = (format->Rmask < format->Bmask) ? GL_RGBA : GL_BGRA;
+	}
+	else {
+		internalFormat = GL_RGB;
+		externalFormat = (format->Rmask < format->Bmask) ? GL_RGB : GL_BGR;
+	}
+
+	glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, tmpSurface->w, tmpSurface->h, 0,
+		externalFormat, GL_UNSIGNED_BYTE, tmpSurface->pixels);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	SDL_FreeSurface(tmpSurface); 
+	return texID;	
+}
+
 void init(void)
 {
+	gouraudShader = rt3d::initShaders("gouraud.vert", "simple.frag");
+	rt3d::setLight(gouraudShader, light0);
+	rt3d::setMaterial(gouraudShader, material0);
 
+	texture = rt3d::initShaders("textured.vert", "textured.frag");
+	skyboxShader = rt3d::initShaders("cubeMap.vert", "cubeMap.frag");
+
+	const char *cubeTexFiles[6] = {
+		"Town-skybox/Town_bk.bmp", "Town-skybox/Town_ft.bmp", "Town-skybox/Town_rt.bmp", "Town-skybox/Town_lf.bmp", "Town-skybox/Town_up.bmp", "Town-skybox/Town_dn.bmp"
+	};
+	loadCubeMap(cubeTexFiles, &skybox[0]);
+
+	vector<GLfloat> verts;
+	vector<GLfloat> norms;
+	vector<GLfloat> tex_coords;
+	vector<GLuint> indices;
+	rt3d::loadObj("cube.obj", verts, norms, tex_coords, indices);
+	cubeIndexCount = indices.size();
+	textures[0] = loadBitmap("fabric.bmp");
+	meshObjects[0] = rt3d::createMesh(verts.size() / 3, verts.data(), nullptr, norms.data(), tex_coords.data(), cubeIndexCount, indices.data());
+
+	textures[2] = loadBitmap("studdedmetal.bmp");
+
+	verts.clear();
+	norms.clear();
+	tex_coords.clear();
+	indices.clear();
+	rt3d::loadObj("bunny-5000.obj", verts, norms, tex_coords, indices);
+	bunnyIndexCount = indices.size();
+	meshObjects[2] = rt3d::createMesh(verts.size() / 3, verts.data(), nullptr, norms.data(), nullptr, bunnyIndexCount, indices.data());
+
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+}
+
+glm::vec3 moveForward(glm::vec3 pos, GLfloat angle, GLfloat d) {
+	return glm::vec3(pos.x + d * std::sin(r*DEG_TO_RADIAN), pos.y, pos.z - d * std::cos(r*DEG_TO_RADIAN));
+}
+
+glm::vec3 moveRight(glm::vec3 pos, GLfloat angle, GLfloat d) {
+	return glm::vec3(pos.x + d * std::cos(r*DEG_TO_RADIAN), pos.y, pos.z + d * std::sin(r*DEG_TO_RADIAN));
 }
 
 void update(void)
 {
-
+	const Uint8 *keys = SDL_GetKeyboardState(NULL);
+	if (keys[SDL_SCANCODE_W]) eye = moveForward(eye, r, 0.1f);
+	if (keys[SDL_SCANCODE_S]) eye = moveForward(eye, r, -0.1f);
+	if (keys[SDL_SCANCODE_A]) eye = moveRight(eye, r, -0.1f);
+	if (keys[SDL_SCANCODE_D]) eye = moveRight(eye, r, 0.1f);
+	if (keys[SDL_SCANCODE_R]) eye.y += 0.1;
+	if (keys[SDL_SCANCODE_F]) eye.y -= 0.1;
 }
 
 void draw(SDL_Window * window)
 {
+	glEnable(GL_CULL_FACE);
+	glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+	glm::mat4 projection(1.0);
+	projection = glm::perspective(float(60.0f*DEG_TO_RADIAN), 800.0f / 600.0f, 1.0f, 150.0f);
+
+	GLfloat scale(1.0f);
+
+	glm::mat4 modelview(1.0);
+	mvStack.push(modelview);
+
+	at = moveForward(eye, r, 1.0f);
+	mvStack.top() = glm::lookAt(eye, at, up);
+
+	glUseProgram(skyboxShader);
+	rt3d::setUniformMatrix4fv(skyboxShader, "projection", glm::value_ptr(projection));
+
+	glDepthMask(GL_FALSE); 
+	glm::mat3 mvRotOnlyMat3 = glm::mat3(mvStack.top());
+	mvStack.push(glm::mat4(mvRotOnlyMat3));
+
+	glCullFace(GL_FRONT);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, skybox[0]);
+	mvStack.top() = glm::scale(mvStack.top(), glm::vec3(1.5f, 1.5f, 1.5f));
+	rt3d::setUniformMatrix4fv(skyboxShader, "modelview", glm::value_ptr(mvStack.top()));
+	rt3d::drawIndexedMesh(meshObjects[0], cubeIndexCount, GL_TRIANGLES);
+	mvStack.pop();
+	glCullFace(GL_BACK);
+
+	glDepthMask(GL_TRUE); 
 }
 
 int main(int argc, char *argv[])
